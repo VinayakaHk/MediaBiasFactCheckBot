168a902706f0d9bce836e12dad77bfbfcf6c9dde
VinayakaHk <32355181+VinayakaHk@users.noreply.github.com>
2023-09-17 15:55:05 +0530

    Create keep_alive.py
    keep alive added

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect
+from threading import Thread
+import logging
+
+app = Flask('keep_alive')
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
+@app.route('/')
+def home():
+  return "I'm alive"
+
+
+def keep_alive():
+  Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
